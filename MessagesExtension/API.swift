5f4b222a54157b45117a1203ac90c51a33946643
james <jamesrochabrun@gmail.com>
2016-12-12 22:27:31 -0800

    bridge

---
+++ MessagesExtension/API.swift
@@ -0,0 +1,32 @@
+//
+//  API.swift
+//  PetMessageApp
+//
+//  Created by James Rochabrun on 12/12/16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+import UIKit
+
+
+
+class API: NSObject {
+    
+    var image: UIImageView!
+    
+    override init() {
+        super.init()
+        
+    }
+    
+    class func requestDataFromQuery(_ query:String)    {
+
+        //let manager = AFHTTPRequestOperationManager()
+        
+        
+    }
+    
+    
+    
+
+}
---
+++ PetMessageApp/PetMessageApp-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  PetMessageApp-Bridging-Header.h
+//  PetMessageApp
+//
+//  Created by James Rochabrun on 12/12/16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+#ifndef PetMessageApp_Bridging_Header_h
+#define PetMessageApp_Bridging_Header_h
+
+#import <AFNetworking/AFNetworking.h>
+
+
+#endif /* PetMessageApp_Bridging_Header_h */
